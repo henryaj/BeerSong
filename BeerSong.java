4708d9ae6354062685ff2bc2f7b0b223b6bbb99f
Henry Stanley <henry@henrystanley.com>
2014-11-10 11:48:12 +0000

    squished bugs - grammar of beer song now correct

--- BeerSong.java
+++ BeerSong.java
@@ -7,7 +7,7 @@ public class BeerSong {
 
       if (beerNum == 1) {
         word = "bottle"; // redefines 'word' to eq bottle
-      }
+      } // end if
 
 
       System.out.println(beerNum + " " + word + " of beer on the wall,");
@@ -15,12 +15,14 @@ public class BeerSong {
       System.out.println("Take one down,");
       System.out.println("pass it around,");
       beerNum = beerNum - 1;
+      System.out.println(beerNum + " " + word + " of beer on the wall.");
+      System.out.println(" ");
 
       if (beerNum > 0) {
-        System.out.println(beerNum + " " + word + " of beer on the wall,")
+        System.out.println(" ");
       } else {
-        System.out.println("No more bottles of beer on the wall!")
-      }
-    }
-  }
-}
+        System.out.println("No more bottles of beer on the wall!");
+      } // end if
+    } // end while
+  } // end main
+} // end BeerSong
